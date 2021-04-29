efb223b30548a724341038ebaf917e07fc7d8f58
Jerry Ng <ngshiheng@gmail.com>
2021-04-29 15:47:37 +0800

    Allow all origin

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
     settings = dict(
         site_title='Burplist',
         debug=args.debug,
+        allowed_origins=['*']
     )
 
     application = tornado.web.Application([
