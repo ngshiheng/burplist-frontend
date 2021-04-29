a334cb4243d7c7c0422dee9b1715bf1a1061f714
Jerry Ng <ngshiheng@gmail.com>
2021-04-29 15:07:36 +0800

    Add 404 page

--- app.py
+++ app.py
@@ -1,29 +1,43 @@
 import argparse
 
+import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 from pywebio.platform.tornado import webio_handler
-from tornado.httpserver import HTTPServer
 
 from src.index import index
 from src.privacy import privacy
 from src.terms import terms
 
-if __name__ == '__main__':
+
+class FourOhFourHandler(tornado.web.RequestHandler):
+    def get(self, slug):
+        self.render('src/templates/404.html')
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--port', type=int, default=8080)
     parser.add_argument('-d', '--debug', type=bool, default=False)
     args = parser.parse_args()
 
-    application = tornado.web.Application(
-        [
-            (r'/', webio_handler(index)),
-            (r'/terms', webio_handler(terms)),
-            (r'/privacy', webio_handler(privacy)),
-        ],
-        debug=args.debug,
+    settings = dict(
+        site_title='Burplist',
+        debug=True,
     )
 
-    http_server = HTTPServer(application)
-    http_server.listen(port=args.port)
+    application = tornado.web.Application([
+        (r'/', webio_handler(index)),
+        (r'/terms', webio_handler(terms)),
+        (r'/privacy', webio_handler(privacy)),
+        (r'/([^/]+)', FourOhFourHandler),
+
+    ], **settings)
+
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(args.port)
     tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main()
