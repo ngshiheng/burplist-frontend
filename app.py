6f750884868113cd349fc4beef07afa189298b7f
Jerry Ng <ngshiheng@gmail.com>
2021-05-01 10:08:34 +0800

    Use webiohandler from pywebio.platform.tornado_http

--- app.py
+++ app.py
@@ -1,133 +1,42 @@
-import argparse
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from pywebio.platform.tornado_http import webio_handler
+from tornado.options import define, options
 
-from pywebio.input import TEXT, input
-from pywebio.output import clear, put_html, put_loading, put_markdown, put_table, put_text, style, use_scope
-from pywebio.platform import seo
-from pywebio.platform.tornado_http import start_server
-from pywebio.session import run_js, set_env
-from sqlalchemy.orm import sessionmaker
+from src.index import index
+from src.privacy import privacy
+from src.terms import terms
 
-from src.utils.models import Product, db_connect
-from src.utils.validators import validate_search_length
-
-engine = db_connect()
-session = sessionmaker(bind=engine)()
+define('port', default=8080, help='Run on the given port', type=int)
+define('debug', default=False, help='Run on the deug mode', type=bool)
 
 
-@seo('Burplist', 'Beer prices at your fingertips')
+class FourOhFourHandler(tornado.web.RequestHandler):
+    def get(self, slug):
+        self.render('src/templates/404.html')
+
+
 def main():
-    set_env(auto_scroll_bottom=False)
-    run_js("""
-    $('head').append('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8667164348741019" crossorigin="anonymous"></script>')
-    $("link[rel*='icon']").attr("href", "https://img.icons8.com/plasticine/100/000000/beer-glass.png");
-    """)
-    put_html(r"""
-    <h1 align="center"><strong>Burplist</strong></h1>
-    """)
-    with use_scope('introduction'):
-        put_html(r"""
-        <style>
-        .img {
-            width: auto;
-            height: auto;
-            max-width: 250;
-            max-height: 280px;
-            border:2px solid #fff;
-            -moz-box-shadow: 10px 10px 5px #ccc;
-            -webkit-box-shadow: 10px 10px 5px #ccc;
-            box-shadow: 10px 10px 5px #ccc;
-            -moz-border-radius:25px;
-            -webkit-border-radius:25px;
-            border-radius:25px;
-        }
-        </style>
+    tornado.options.parse_command_line()
 
-        <p align="center">
-            <img class="img" width="50%" height="50%" src="https://media.giphy.com/media/l3c5RJr6yRKyyIw00/giphy.gif">
-        </p>
-        """)
+    settings = dict(
+        site_title='Burplist',
+        debug=options.debug,
+    )
 
-        put_markdown(r"""
-        # What is this?
-        🇸🇬 A collection of craft beer prices in Singapore.
-        🖐 Prices of **all** beers in Singapore at your fingertips.
+    app = tornado.web.Application([
+        (r'/', webio_handler(index)),
+        (r'/terms', webio_handler(terms)),
+        (r'/privacy', webio_handler(privacy)),
+        (r'/([^/]+)', FourOhFourHandler),
 
-        # What is craft beer?
-        🤤 To simply put, craft beers are the more **delicious** alternative to your mainstream beers.
-        🍻 In terms of styles, flavours and aroma, craft beers are usually more **diverse** in these aspects.
-        💁‍♂️ Craft beers are usually brewed in smaller quantities by passionate brewers who care more about **quality** than quantity.
-        """, lstrip=True)
+    ], **settings)
 
-    while True:
-        search = input(
-            type=TEXT,
-            required=True,
-            label='🤩 Start looking here:',
-            placeholder='Search for a beer name...',
-            help_text='Try: "Hitachino Nest White Ale"',
-            validate=validate_search_length,
-        )
-        clear('introduction')
-        clear('result')
-
-        # NOTE: Because the underlying SQL is using `to_tsquery`, we have to wrap our search text with single quotes
-        with style(put_loading(color='primary'), 'width:20rem; height:20rem; display:block; margin-left:auto; margin-right:auto;'):
-            products = session.query(Product) \
-                .filter(Product.name.match(f"'{search}'")) \
-                .order_by(Product.price_per_quantity.asc()) \
-                .all()
-            session.close()
-
-            with use_scope('result'):
-                if not products:
-                    put_html(r"""
-                     <style>
-                    .img {
-                        width: auto;
-                        height: auto;
-                        max-width: 250;
-                        max-height: 280px;
-                        border:2px solid #fff;
-                        -moz-box-shadow: 10px 10px 5px #ccc;
-                        -webkit-box-shadow: 10px 10px 5px #ccc;
-                        box-shadow: 10px 10px 5px #ccc;
-                        -moz-border-radius:25px;
-                        -webkit-border-radius:25px;
-                        border-radius:25px;
-                    }
-                    </style>
-                    <p align="center">
-                        <img class="img" width="50%" height="50%" src="https://media.giphy.com/media/BEob5qwFkSJ7G/giphy.gif">
-                    </p>""")
-                    put_html(f'<h2 align="center">😢 Oh no, we couldn\'t find anything relevant to "{search}"...</h2>')
-                    continue
-
-                put_html(f"""
-                <h2 align="center">🔍 Here are your results for "{search}"...</h2>
-                """)
-                # Display the final result in a table
-                put_table(
-                    tdata=[
-                        [
-                            put_html(f'<a href="{product.url}" target="_blank">{product.name}</a>'),
-                            f'{product.last_price:.2f}',
-                            product.quantity,
-                            style(put_text(f'{product.price_per_quantity:.2f}'), 'color:red'),
-                        ] for product in products
-                    ],
-                    header=[
-                        'Name',
-                        'Price ($SGD)',
-                        'Qty.',
-                        'Price/Qty. ($SGD)',
-                    ],
-                )
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=8080)
-    parser.add_argument("-d", "--debug", type=bool, default=False)
-    args = parser.parse_args()
-
-    start_server(main, port=args.port, debug=args.debug)
+    main()
