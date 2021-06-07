92a932508798014df03b0a415ced49c6c1661fde
Jerry Ng <ngshiheng@gmail.com>
2021-06-07 09:16:44 +0800

    Add CSV download via gumroad

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -10,7 +10,7 @@ $('head').append('<link rel="icon" type="image/x-icon" href="{favicon_url}">')
 """
 
 footer = f"""
-$('footer').html('📬 <a href="mailto:{mail_to}">Contact Us</a> | 📃 <a href="/terms">Terms of Use</a> | 🔏 <a href="/privacy">Privacy Policy</a>')
+$('footer').html('👇 <a href="https://gumroad.com/l/burplist">Download CSV</a> | ✉️ <a href="mailto:{mail_to}">Contact</a> | 📃 <a href="/terms">Terms of Use</a> | 🔏 <a href="/privacy">Privacy Policy</a>')
 """
 
 landing_page_heading = r"""
@@ -42,7 +42,7 @@ landing_page_gif = r"""
 """
 
 landing_page_description = r"""
-# What is this?
+# What is Burplist?
 🇸🇬 A collection of **craft beer** prices in Singapore at your fingertips.
 🔎 Think of it as a **search engine** for craft beers in Singapore.
 
@@ -54,11 +54,15 @@ landing_page_description = r"""
 # How to use?
 ✍️ Simply enter any beer _brand_, _style_, or _name_ that you want in the search bar and hit "Submit".
 🤑 Prices are ordered starting **from the lowest** to highest.
+
+# Download full CSV
+👇 Gain full access to **over 2,000** unique craft beers price and details for as low as **SGD$2**.
+🙌 Your support will help to keep this free site running.
 """
 
 
 product_not_found_gif = r"""
-    <style>
+<style>
 .img {
     width: auto;
     height: auto;
@@ -81,4 +85,9 @@ product_not_found_gif = r"""
 
 amplitude_tracking = r"""
 $('head').append('<script type="text/javascript">(function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script") ;r.type="text/javascript" ;r.integrity="sha384-u0hlTAJ1tNefeBKwiBNwB4CkHZ1ck4ajx/pKmwWtc+IufKJiCQZ+WjJIi+7C6Ntm" ;r.crossOrigin="anonymous";r.async=true ;r.src="https://cdn.amplitude.com/libs/amplitude-8.1.0-min.gz.js" ;r.onload=function(){if(!e.amplitude.runQueuedFunctions){console.log("[Amplitude] Error: could not load SDK")}};var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i) ;function s(e,t){e.prototype[t]=function(){this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this}}var o=function(){this._q=[];return this};var a=["add","append","clearAll","prepend","set","setOnce","unset","preInsert","postInsert","remove"] ;for(var c=0;c<a.length;c++){s(o,a[c])}n.Identify=o;var u=function(){this._q=[] ;return this};var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"] ;for(var p=0;p<l.length;p++){s(u,l[p])}n.Revenue=u ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","enableTracking","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"] ;function v(e){function t(t){e[t]=function(){e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){e=(!e||e.length===0?"$default_instance":e).toLowerCase() ;if(!Object.prototype.hasOwnProperty.call(n._iq,e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]};e.amplitude=n})(window,document); amplitude.getInstance().init("b5c55c2974a58b1c85cdd80332431d44");</script>')
+"""
+
+gumroad_embedded_link = r"""
+<script src="https://gumroad.com/js/gumroad.js"></script>
+<a class="gumroad-button" href="https://gumroad.com/l/NsZiD" target="_blank">Click here to download</a>
 """
